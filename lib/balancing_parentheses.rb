c88b53ddaee7d77617b95deb3ce97d6f181a30c2
Aaron Noe <aarminoe@gmail.com>
2022-07-22 20:23:53 -0500

    commit tree 6cadd8c21774397e40f7ba32027c84bba344854e

--- lib/balancing_parentheses.rb
+++ lib/balancing_parentheses.rb
@@ -1,3 +1,18 @@
 require_relative './stack'
 
-# your code here
+def balancing_parentheses(string)
+    stack = Stack.new
+    opens = 0
+    string.chars.each do |char|
+        if char == '('
+            stack.push(char)
+            opens += 1
+        elsif char == ')' && opens > 0
+            opens -=1
+            stack.pop
+        else 
+            stack.push(char)
+        end
+    end
+    stack.size
+end
